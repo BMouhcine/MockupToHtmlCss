5a6dae67ed56f07f22133a5f9e8617f06df47357
BMouhcine <44417293+BMouhcine@users.noreply.github.com>
2020-04-10 23:10:38 +0100

    PCted
    nthng
    
    Co-Authored-By: Mohamed ouokki <mohamed.ouokki@um5s.net.ma>

---
+++ mockup2html/src/test/java/com/project/mockup2html/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mockup2html;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
