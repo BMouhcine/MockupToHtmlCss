9d4ec018a6dab854134dbfd1036fe66e682b2fb8
Ouokki <ouokki98@gmail.com>
2020-05-07 04:45:05 +0100

    Updates ViewController Spring

--- src/main/java/com/project/mockup2html/Controllers/ViewController.java
+++ src/main/java/com/project/mockup2html/Controllers/ViewController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ViewController {
 	
 	//Login Register Handler
-		@GetMapping("/")
+		@GetMapping("/about")
 	    public String portalAngularHandler() {
 	        return "forward:./index.html";
 	    }
