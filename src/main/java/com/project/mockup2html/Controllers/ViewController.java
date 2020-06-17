2dd11570964b829b2eae0b3cb334d51bc0171509
BMouhcine <44417293+BMouhcine@users.noreply.github.com>
2020-06-17 11:19:25 +0100

    Sigma Stuffs

--- src/main/java/com/project/mockup2html/Controllers/ViewController.java
+++ src/main/java/com/project/mockup2html/Controllers/ViewController.java
@@ -1,14 +1,9 @@
 package com.project.mockup2html.Controllers;
 
 
-import java.awt.print.Printable;
-
-import org.opencv.core.Mat;
-import org.opencv.imgcodecs.Imgcodecs;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import nu.pattern.OpenCV;
 
 
 @Controller
@@ -17,10 +12,8 @@ public class ViewController {
 	//Login Register Handler
 		@GetMapping("/login")
 	    public String portalAngularHandler() {
-			OpenCV.loadShared();
-			System.out.println("loooooooooooooooooooooooooooooooooooooooooool");
 	        return "forward:./index.html";
 		}
 		
-		
+	
 }
