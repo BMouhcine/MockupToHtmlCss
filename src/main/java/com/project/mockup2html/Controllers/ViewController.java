d20eec3ec5be86633eb812d0154ef65b141c8fb4
BMouhcine <44417293+BMouhcine@users.noreply.github.com>
2020-06-17 11:06:43 +0100

    Σ opencv stuffs

--- src/main/java/com/project/mockup2html/Controllers/ViewController.java
+++ src/main/java/com/project/mockup2html/Controllers/ViewController.java
@@ -1,8 +1,14 @@
 package com.project.mockup2html.Controllers;
 
 
+import java.awt.print.Printable;
+
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import nu.pattern.OpenCV;
 
 
 @Controller
@@ -11,7 +17,10 @@ public class ViewController {
 	//Login Register Handler
 		@GetMapping("/login")
 	    public String portalAngularHandler() {
+			OpenCV.loadShared();
+			System.out.println("loooooooooooooooooooooooooooooooooooooooooool");
 	        return "forward:./index.html";
 		}
+		
 		
 }
