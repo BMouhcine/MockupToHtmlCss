3289a37c9bf4d44a940a10ad8f1d0baebbb54768
BMouhcine <44417293+BMouhcine@users.noreply.github.com>
2020-06-17 16:06:54 +0100

    sum stuff

--- src/main/java/com/project/mockup2html/Controllers/ImageController.java
+++ src/main/java/com/project/mockup2html/Controllers/ImageController.java
@@ -84,12 +84,14 @@ public class ImageController {
 		Mat bgImgMat = Mat.ones(256, 256, CvType.CV_16U);Core.repeat(bgImgMat, 3, 3, bgImg);
 		
 		Core.multiply(bgImg, new Scalar(255, 255, 255), bgImg);
-		Core.multiply(imgResizedMat, bgImg, imgResizedMat);
-		/*
-		 *  for(int i=27;i<227;i++) {
-		 * for(int j=27;j<227;j++) { bgImg.put(i, j, imgResizedMat.get(i-27, j-27)); } }
-		 * Core.divide(bgImg,new Scalar(255, 255, 255), bgImg);
-		 */
+		
+		  for(int i=27;i<227;i++) {
+			  for(int j=27;j<227;j++) { 
+				  bgImg.put(i, j, imgResizedMat.get(i-27, j-27)); 
+				  }
+			  }
+		  Core.divide(bgImg,new Scalar(255, 255, 255), bgImg);
+		 
 		
 		
 		return bgImg;
