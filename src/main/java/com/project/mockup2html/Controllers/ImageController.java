d9ad47fac4d07fdf2557cb3f844234f6f7a9e991
BMouhcine <44417293+BMouhcine@users.noreply.github.com>
2020-06-17 12:18:41 +0100

    k

--- src/main/java/com/project/mockup2html/Controllers/ImageController.java
+++ src/main/java/com/project/mockup2html/Controllers/ImageController.java
@@ -2,6 +2,12 @@ package com.project.mockup2html.Controllers;
 
 
 import java.io.IOException;
+
+import javax.imageio.IIOImage;
+
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +21,8 @@ import com.project.mockup2html.Models.ImageUI;
 import com.project.mockup2html.Models.User;
 import com.project.mockup2html.Repositories.ImageUIRepository;
 import com.project.mockup2html.Repositories.UserRepository;
+
+import nu.pattern.OpenCV;
 
 @RestController
 @RequestMapping("/")
@@ -33,10 +41,20 @@ public class ImageController {
         System.out.println("File name"+givenImage.getOriginalFilename()+" uploaded successfully by "+user_id+".");
         if(UserController.currentUserId!=-1) {
         	User userBuffer = userRepository.findById(UserController.currentUserId).get();
+        	OpenCV.loadShared();
             imageUIRepository.save(new ImageUI(givenImage.getBytes(),userBuffer));
+            
+            Mat imgMat = Imgcodecs.imdecode(new MatOfByte(givenImage.getBytes()), Imgcodecs.CV_LOAD_IMAGE_UNCHANGED);
+            String dataString= String.valueOf(imgMat.size().width)+" : "+String.valueOf(imgMat.size().height)+"\n Path: "+ this.getClass().getClassLoader().getResource("static/assets/upload-icon.png").getFile();
             
         }
         return ResponseEntity.ok().body(null);
     }
+	
+	
+	public static Mat loadImage(String imagePath) {
+	    Imgcodecs imageCodecs = new Imgcodecs();
+	    return imageCodecs.imread(imagePath);
+	}
 	
 }
--- src/main/java/com/project/mockup2html/Controllers/ViewController.java
+++ src/main/java/com/project/mockup2html/Controllers/ViewController.java
@@ -5,29 +5,19 @@ import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import nu.pattern.OpenCV;
 
 
 
-@RestController
+
+@Controller
 public class ViewController {
 	
 	//Login Register Handler
 		@GetMapping("/login")
-	    public String portalAngularHandler() {
-			OpenCV.loadShared();
-			Mat imgMat = loadImage(this.getClass().getClassLoader().getResource("static/assets/upload-icon.png").getFile());
-			String dataString= String.valueOf(imgMat.size().width)+" : "+String.valueOf(imgMat.size().height);
-			dataString = dataString + "\n Path: "+ this.getClass().getClassLoader().getResource("static/assets/upload-icon.png").getFile();
+	    public String portalAngularHandler() { 
 			
-			return dataString;
-	        //return "forward:./index.html";
+	        return "forward:./index.html";
 		}
 		
-		public static Mat loadImage(String imagePath) {
-		    Imgcodecs imageCodecs = new Imgcodecs();
-		    return imageCodecs.imread(imagePath);
-		}
+		
 }
