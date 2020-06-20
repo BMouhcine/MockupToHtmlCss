24741cfba658526ebeaf5bab4babf4fa46234c66
Ouokki <ouokki98@gmail.com>
2020-06-20 15:53:45 +0100

    Updates

--- client/src/app/service/projects-fetch.service.ts
+++ client/src/app/service/projects-fetch.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { map } from "rxjs/operators";
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +9,5 @@ export class ProjectsFetchService {
   
   constructor(private http: HttpClient) { }
 
-  FetchProjects(user_id){
-    return this.http.get<any>('/requete',{
-      params:new HttpParams().set('id',user_id)
-    }).pipe(
-      map((res:any) => {
-        console.log(res)
-        return res;
-      })
-    );
-  }
+  
 }
