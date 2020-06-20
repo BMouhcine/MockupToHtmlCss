e0a9f6a5ccd87399aa998f8cbc01f5cb754e6caa
nada28 <31458143+nada28@users.noreply.github.com>
2020-06-20 23:19:44 +0000

    Fetching projects, preparing html file to be previewed and downloaded

--- client/src/app/Pages/projects/projects.component.ts
+++ client/src/app/Pages/projects/projects.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ProjectsFetchService } from '../../service/projects-fetch.service';
-
+import { saveAs } from 'file-saver';
+ 
+ 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
-  Projects :any 
+  Projects :any
   constructor(private projectsservice: ProjectsFetchService,private titleService:Title) {
     this.titleService.setTitle("Projects");
    }
@@ -18,9 +20,32 @@ export class ProjectsComponent implements OnInit {
     //the FetchProjetcs Function
       this.projectsservice.FetchProjects(sessionStorage.getItem("id"))
            .subscribe(data => {
-                this.Projects = data.total;
+ 
+                this.Projects = data;
+                console.log(this.Projects[0]);
        })
 }
-
-
+preview(project:any) {
+  console.log(typeof(project));
+  let blob = new Blob([project], {type: 'text/html'});
+        let url = window.URL.createObjectURL(blob);
+        let pwa = window.open(url);
+        if (!pwa || pwa.closed || typeof pwa.closed == 'undefined') {
+            alert( 'Please disable your Pop-up blocker and try again.');
+        }
+       
+  }
+  download(project:any) {
+    console.log(typeof(project));
+    let blob = new Blob([project], {type: 'text/html'});
+          let url = window.URL.createObjectURL(blob);
+          let pwa = window.open(url);
+          if (!pwa || pwa.closed || typeof pwa.closed == 'undefined') {
+              alert( 'Please disable your Pop-up blocker and try again.');
+          }
+          saveAs.saveAs(blob, "hello world.html");
+         
+    }
+ 
+ 
 }
