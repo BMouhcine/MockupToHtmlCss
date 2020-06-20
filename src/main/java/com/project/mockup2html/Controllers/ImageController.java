687edd8fe00278b31459119d6bb84b4f4e4e4b49
nada28 <31458143+nada28@users.noreply.github.com>
2020-06-20 22:50:19 +0000

    s

--- src/main/java/com/project/mockup2html/Controllers/ImageController.java
+++ src/main/java/com/project/mockup2html/Controllers/ImageController.java
@@ -1,6 +1,6 @@
 package com.project.mockup2html.Controllers;
-
-
+ 
+ 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
@@ -9,7 +9,7 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpEntity;
@@ -29,48 +29,48 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
-
+ 
 import com.project.mockup2html.Models.Code;
 import com.project.mockup2html.Models.ImageUI;
 import com.project.mockup2html.Models.User;
 import com.project.mockup2html.Repositories.CodeRepository;
 import com.project.mockup2html.Repositories.ImageUIRepository;
 import com.project.mockup2html.Repositories.UserRepository;
-
+ 
 import net.minidev.json.JSONObject;
-
-
+ 
+ 
 @RestController
 @RequestMapping("/")
 @CrossOrigin("*")
 public class ImageController {
-	@Autowired
-	ImageUIRepository imageUIRepository;
-
-	@Autowired
-	UserRepository userRepository;
-
-	@Autowired
-	CodeRepository codeRepository;
-
-	@Autowired
-	RestTemplate restTemplate;
-
-
-	@PostMapping(value = "/doUploadImage", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Autowired
+    ImageUIRepository imageUIRepository;
+ 
+    @Autowired
+    UserRepository userRepository;
+ 
+    @Autowired
+    CodeRepository codeRepository;
+ 
+    @Autowired
+    RestTemplate restTemplate;
+ 
+ 
+    @PostMapping(value = "/doUploadImage", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> uploadImageHandler(@RequestParam MultipartFile givenImage, @RequestParam long user_id) throws IOException {
-
+ 
         if(UserController.currentUserId!=-1) {
-        	System.out.println("File name"+givenImage.getOriginalFilename()+" uploaded successfully by "+user_id+".");
-        	User userBuffer = userRepository.findById(UserController.currentUserId).get();
-        	ImageUI imageUI = new ImageUI(givenImage.getBytes(),userBuffer);
+            System.out.println("File name"+givenImage.getOriginalFilename()+" uploaded successfully by "+user_id+".");
+            User userBuffer = userRepository.findById(UserController.currentUserId).get();
+            ImageUI imageUI = new ImageUI(givenImage.getBytes(),userBuffer);
             imageUIRepository.save(imageUI);
-
+ 
             HttpHeaders headers = new HttpHeaders();
-        	headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-
-        	MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-        	java.io.File file = new java.io.File("file");
+            headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+ 
+            MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+            java.io.File file = new java.io.File("file");
             OutputStream os = new FileOutputStream(file);
             os.write(givenImage.getBytes());
             os.close();
@@ -79,40 +79,47 @@ public class ImageController {
             // build the request
             HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
             // send POST request
-
+ 
             ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:5000/post/", requestEntity, String.class);
             String buff = response.getBody();
             if(response.getStatusCode()==HttpStatus.OK) {
-
-            	Code code = new Code(buff, imageUI, userBuffer);
-
-            	codeRepository.save(code);
-				/*
-				 * File codeFile = new File("code.html"); FileWriter codeWriter = new
-				 * FileWriter(codeFile); codeWriter.write(code.getHtmlcode());
-				 * codeWriter.close();
-				 */
-
-            	return ResponseEntity.ok().body(buff);
-
+ 
+                Code code = new Code(buff, imageUI, userBuffer);
+ 
+                codeRepository.save(code);
+                /*
+                 * File codeFile = new File("code.html"); FileWriter codeWriter = new
+                 * FileWriter(codeFile); codeWriter.write(code.getHtmlcode());
+                 * codeWriter.close();
+                 */
+ 
+                return ResponseEntity.ok().body(buff);
+ 
             }else {
-            	return ResponseEntity.ok().body("error");
+                return ResponseEntity.ok().body("error");
             }
-
-
+ 
+ 
         }
-
+ 
         return ResponseEntity.ok().body(null);
     }
-	@PostMapping(value = "/getAllCodes")
-	public ResponseEntity<List<Code>> getAllCodes(@RequestParam long user_id){
-		if(UserController.currentUserId!=-1) {
-			User userBuffer = userRepository.findById(UserController.currentUserId).get();
-
-			return ResponseEntity.ok().body(codeRepository.findAllByUserCode(userBuffer));
-		}
-		return ResponseEntity.ok().body(null);
-	}
-
-
+    @PostMapping(value = "/getAllCodes")
+    public ResponseEntity<List<String>> getAllCodes(){
+        if(UserController.currentUserId!=-1) {
+            User userBuffer = userRepository.findById(UserController.currentUserId).get();
+            List<Code> c = codeRepository.findAllByUserCode(userBuffer);
+            System.out.println(c.get(0).getHtmlcode()+"____________________"+c.get(1).getHtmlcode());
+            List<String> list2Return = new ArrayList<String>();
+            for(Code e: c) {
+                list2Return.add(e.getHtmlcode());
+            }
+           
+            return ResponseEntity.ok().body(list2Return);
+           
+        }
+        return ResponseEntity.ok().body(null);
+    }
+ 
+ 
 }
